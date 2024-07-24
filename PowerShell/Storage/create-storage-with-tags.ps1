7b67cf9e32c3edb8ce11280e9b3807b364d39bc2
Carlos Henrique Ramos <carlos.hramos@outlook.com>
2024-07-24 11:22:45 -0300

    add create-storage-with-tags

---
+++ PowerShell/Storage/create-storage-with-tags.ps1
@@ -0,0 +1,76 @@
+<#
+.SYNOPSIS
+    Cria Storage Account
+
+.DESCRIPTION
+    Cria Storage Account
+
+.EXAMPLE
+    .\create-storage-with-tags.ps1
+
+.NOTES
+    Nome: create-storage-with-tags
+    Versão 1.0.0
+    Autor: Carlos Henrique | Azure Cloud Specialist | Azure Infrastructure
+    Linkedin: https://www.linkedin.com/in/carloshenriqueramos
+    E-mail: carlos.hramos@outlook.com
+
+.LINK
+    https://github.com/carloshenriqueramos/Azure/blob/main/PowerShell/Storage/create-storage-with-tags.ps1
+#>
+
+# Connect ao Azure
+#Connect-AzAccount
+
+# Solicita as informacoes de Resource Group e nome da Storage Account
+$resourceGroupName = read-host "Informe o nome do Resource Group"
+$storageAccountName = read-host "Informe o nome da Storage Account"
+$location = read-host "Informe a Localizacao do Resource Group e Storage Account"
+$sku = read-host "Informe o SKU da Storage Account"
+
+# Opcoes                                            SkuName 
+#Locally redundant storage (LRS)	                 = Standard_LRS
+#Zone-redundant storage (ZRS)	                    = Standard_ZRS
+#Geo-redundant storage (GRS)	                    = Standard_GRS
+#Read-access geo-redundant storage (RAGRS)	     = Standard_RAGRS
+#Geo-zone-redundant storage (GZRS)	              = Standard_GZRS
+#Read-access geo-zone-redundant storage (RA-GZRS) = Standard_RAGZRS
+
+# Definicao de Tags
+$tags = @{
+
+            "ACN"="";
+            "AMBIENTE"="";
+            "ANOCRIACAO"="";
+            "APLICACAO"="";
+            "CC"="";
+            "COMPARTILHADO"="";
+            "DIRETORIA"="";
+            "EMPRESA"=""
+            "INICIATIVA"=""
+         }
+
+# Valida se o nome desejado da Storage Account esta disponivel
+$saNameAvailable = (Get-AzStorageAccountNameAvailability -Name $storageAccountName).NameAvailable
+
+if ($saNameAvailable) {
+   
+   # Cria Storage Account
+   New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+   -Name $storageAccountName `
+   -Location $location `
+   -SkuName $sku `
+   -Kind StorageV2 `
+   -Tag $tags `
+   -EnableHttpsTrafficOnly $true `
+   -AllowBlobPublicAccess $false `
+   -MinimumTlsVersion TLS1_2
+
+   Write-host "Storage Account" $storageAccountName "criada!" -BackgroundColor Green
+
+} else
+   
+   {
+      Write-host "Nome" $storageAccountName "ja esta em uso, por favor escolha um novo nome" -BackgroundColor Red
+      Exit 1
+   }
