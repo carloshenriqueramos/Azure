72373f1be2302ded6039209859e88f8a9eafb320
Carlos Henrique Ramos <carlos.hramos@outlook.com>
2024-07-24 14:50:42 -0300

    add get-all-resource-group-empty

---
+++ PowerShell/Resource Group/get-all-resource-group-empty.ps1
@@ -0,0 +1,65 @@
+<#
+.SYNOPSIS
+    Lista todos os Resource Groups sem Recursos
+
+.DESCRIPTION
+    Lista todos os Resource Groups sem Recursos
+
+.EXAMPLE
+    .\get-all-resource-group-empty.ps1
+
+.NOTES
+    Nome: get-all-resource-group-empty
+    Versão 1.0.0
+    Autor: Carlos Henrique | Azure Cloud Specialist | Azure Infrastructure
+    Linkedin: https://www.linkedin.com/in/carloshenriqueramos
+    E-mail: carlos.hramos@outlook.com
+
+.LINK
+    https://github.com/carloshenriqueramos/Azure/blob/main/PowerShell/Storage/get-all-resource-group-empty.ps1
+#>
+
+# Connect ao Azure
+Connect-AzAccount
+
+# Guardando dados no Array
+$output = @()
+
+# Definindo diretorio de destino do export do arquivo CSV
+$outputCsv = "C:\TEMP\ResourceGroupsEmpty.csv"
+
+# Get de todos os Resource Groups
+$rgs = Get-AzResourceGroup
+
+# Analisando cada Resource Group
+foreach ($rg in $rgs){
+	
+	# Obtem os recursos existentes no Resource Group
+	$resources = Get-AzResource -ResourceGroupName $rg.ResourceGroupName 
+	
+	# Valida se o Resource Group esta vazio
+	if ($resources -eq $null) {
+			
+		# Analisando cada Resource Group Vazio
+		ForEach ($rgempty in $rg){
+
+			# Criando estrutura para para o arquivo de Export
+			$outputObject = [PSCustomObject]@{
+				
+				ResourceGroupEmpty = $rg.ResourceGroupName
+
+			}
+			
+			# Adicionando os objetos de consulta no Array de saida
+			$output += $outputObject 
+
+		}
+	
+	}
+		
+}
+
+# Export para o arquivo CSV
+$output | Export-Csv -Path $outputCsv -NoTypeInformation
+
+Write-Output "Export completed. CSV file saved to $outputCsv"
